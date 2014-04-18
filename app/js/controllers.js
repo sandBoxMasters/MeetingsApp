794af93063e99c8ec20c318323caf8e79e4a4a7f
mrk9 <marko.hujski@gmail.com>
2014-04-19 00:45:37 +0200

    some views are changed and REST api is added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,34 +21,58 @@ angular.module('meetingsApp.controllers', [])
 
   }])
 
-  .controller('profileCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/user.json').success(function(data) {
-      $scope.user = data;
+  .controller('profileCtrl', ['$scope', 'User',
+  function ($scope, User) {
+    $scope.user = User.query();
+  }])
+
+    .controller('eventsCtrl', ['$scope', 'UserEvents',
+  function ($scope, UserEvents) {
+    $scope.user = UserEvents.query();
+  }])
+
+.controller('groupsCtrl', ['$scope', 'Groups',
+  function ($scope, Groups) {
+    $scope.groups = Groups.query();
+  }])
+
+.controller('groupDetailCtrl', ['$scope', '$routeParams', 'Group',
+  function ($scope, $routeParams, Group) {
+    $scope.group = Group.get({GID: $routeParams.GID});
+  }])
+
+;
+
+/**
+  TODO:
+  Pametno bi blo spremenit eventsCtrl, da bo mel scope userMeetings al neki in ne user, in potem ustrezno pohendlat po viewjih!
+
+  STARI KONTROLERJI
+  .controller('groupDetailCtrl', ['$scope', '$routeParams', '$http',
+  function ($scope, $routeParams, $http) {
+    $http.get('data/groupDetail/'+ $routeParams.GID +'.json').success(function(data) {
+      $scope.group = data;
     });
   }])
 
-    .controller('eventsCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/usermeetings.json').success(function(data) {
-      $scope.user = data;
-    });
-  }])
-
-.controller('groupsCtrl', ['$scope', '$http',
+  .controller('groupsCtrl', ['$scope', '$http',
   function ($scope, $http) {
     $http.get('data/usergroup.json').success(function(data) {
       $scope.groups = data;
     });
   }])
 
-.controller('groupDetailCtrl', ['$scope', '$routeParms', '$http',
-  function ($scope, $routeParms, $http) {
-    $http.get('data/'+$routeParms.GID+'.json').success(function(data) {
-      $scope.groupDetail = data;
+  .controller('eventsCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('data/usermeetings.json').success(function(data) {
+      $scope.user = data;
     });
   }])
 
-
-
-;
+  .controller('profileCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('data/user.json').success(function(data) {
+      $scope.user = data;
+    });
+  }])
+**/
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,36 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('meetingsApp.services', []).
+/*angular.module('meetingsApp.services', []).
   value('version', '0.1');
+*/
+
+var meetingsAppServices = angular.module('meetingsApp.services', ['ngResource']);
+
+meetingsAppServices
+	.factory('Group', ['$resource',
+		function($resource){
+			return $resource('data/groupDetail/:GID.json', {}, {
+			query: {method:'GET', params:{GID:'group'}, isArray:false}
+		});
+	}])
+	.factory('Groups', ['$resource',
+		function($resource){
+			return $resource('data/usergroup.json', {}, {
+			query: {method:'GET', params:{}, isArray:false}
+		});
+	}])
+	.factory('UserEvents', ['$resource',
+		function($resource){
+			return $resource('data/usermeetings.json', {}, {
+			query: {method:'GET', params:{}, isArray:false}
+		});
+	}])
+	.factory('User', ['$resource',
+		function($resource){
+			return $resource('data/user.json', {}, {
+			query: {method:'GET', params:{}, isArray:false}
+		});
+	}])
+
+;
