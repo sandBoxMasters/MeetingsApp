3d6b268430578f25c62329ced0577a6777a70b5d
metodribic <metod.ribic@gmail.com>
2014-05-21 22:26:42 +0200

    login&events

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,25 +1,47 @@
 'use strict';
 
 /* Controllers */
+var user_id;
 
-angular.module('meetingsApp.controllers', ['firebase'])
+angular.module('meetingsApp.controllers', ['firebase', 'ngRoute'])
   .controller('homeCtrl', [function($scope) {
 
   }])
-  .controller('loginCtrl', ['$scope', '$rootScope', '$firebaseSimpleLogin', function($scope, $rootScope, $firebaseSimpleLogin) {
+
+/* -------------------------------------------------- login controler ---------------------------------------------------------------------- */  
+
+  .controller('loginCtrl', ['$scope', '$firebaseSimpleLogin', 'UserSession', '$location', function($scope, $firebaseSimpleLogin, UserSession, $location) {
     var ref = new Firebase('https://scorching-fire-5198.firebaseio.com');
-    $rootScope.auth = $firebaseSimpleLogin(ref);
-    $scope.signIn = function () {
-      $rootScope.auth.$login('password', {
-        email: $scope.email,
-        password: $scope.password
-      }).then(function(user) {
+    var auth = new FirebaseSimpleLogin(ref, function(error, user)
+    {
+      if(error)
+      {
+        console.log(error);
+      }
+      else if(user)
+      {
+        user_id = user.uid;
+        UserSession.login(user);
+        $location.path('/events');
         console.log('Logged in as: ', user.uid);
-      }, function(error) {
-        console.error('Login failed: ', error);
-      });
+      }
+      else { console.log('user is logged out'); }
+    });
+  
+    $scope.LoginEP = function()
+    {
+
+      auth.login('password',
+        {
+          email: $scope.email,
+          password: $scope.password
+        });
     }
   }])
+
+
+/* -------------------------------------------------- end of login controler ---------------------------------------------------------------------- */
+
   .controller('registerCtrl', [function($scope) {
 
   }])
@@ -33,36 +55,55 @@ angular.module('meetingsApp.controllers', ['firebase'])
   }])
 
   .controller('profileCtrl', ['$scope', 'User',
-  function ($scope, User) {
-    $scope.user = User.query();
+    function ($scope, User) {
+      $scope.user = User.query();
   }])
 
-  /** ---------- calendar controler ---------- **/
-    .controller('eventsCtrl', ['$scope', 'UserEvents',
-  function ($scope, UserEvents) {
-    $scope.user = UserEvents.query();
+/* -------------------------------------------------- calendar controler ---------------------------------------------------------------------- */
 
-    $(document).ready(function() {
-      
+  .controller('eventsCtrl', ['$scope', 'UserEvents', 'UserSession',
+    function($scope, UserEvents, UserSession) {
+      var testObj = new Firebase('https://scorching-fire-5198.firebaseio.com/meetingTable');
+      //console.log(testObj.toSource());
+      //console.log(user_id);
+      var dbase = new Firebase('https://scorching-fire-5198.firebaseio.com/meetingTable/Master Meeting');
+      dbase.on('value', function(meeting){
+        var meetingObj = {title: meeting.val()['meetingDescription'], start: meeting.val()['dateTimeStart'], end: meeting.val()['dateTimeEnd']};
+        
+       
         var date = new Date();
         var d = date.getDate();
         var m = date.getMonth();
         var y = date.getFullYear();
-        
-        $('#calendar').fullCalendar({
-          header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-          },
-          editable: true,
-          
-          
-        });
-        
-      });
 
-  }])
+                    
+        $('#calendar').fullCalendar({
+            header: {
+              left: 'prev,next today',
+              center: 'title',
+              right: 'month,agendaWeek,agendaDay',
+            },
+            editable: true,
+            disableDragging: true,
+
+            eventSources: [{
+                events: [{
+                  title: meetingObj.title,
+                  start: meetingObj.start,
+                  end: meetingObj.end
+                }],
+
+                type: 'GET',
+                color: 'yellow',     // an option!
+                textColor: 'black' // an option!
+            }]  
+        });
+
+      });
+    }
+  ])
+
+/* -------------------------------------------------- end of calendar controler ------------------------------------------------------------- */
 
 .controller('groupsCtrl', ['$scope', 'Groups',
   function ($scope, Groups) {
@@ -73,39 +114,39 @@ angular.module('meetingsApp.controllers', ['firebase'])
   function ($scope, $routeParams, Group) {
     $scope.group = Group.get({GID: $routeParams.GID});
   }])
+/*----------------------------------------------------------------------------------------------------------------------------------*/
+
+.controller('navCtrl', function($scope, UserSession, $firebase, $location){
+    $scope.$watch(UserSession.isAuthenticated, function() {
+      //$scope.auth = $cookieStore.get('loggedIn');
+      $scope.userL = UserSession.getUserL();
+      //$scope.userB = UserSession.getUserB();
+    });
+  
+    var dataRef = new Firebase("https://scorching-fire-5198.firebaseio.com");
+    var auth = new FirebaseSimpleLogin(dataRef, function(error, user){
+      if (user){
+        user_id = user.uid;
+        $scope.isUserLoggedIn = true;
+      } else {
+        $scope.isUserLoggedIn = false;
+      }
+    });
+    $scope.logout = function(){
+      auth.logout();
+      UserSession.setAuthenticated(false);
+      $location.path('/home');
+      $scope.isUserLoggedIn = false;
+
+    }
+  })
 
 ;
+
+
 
 /**
   TODO:
   Pametno bi blo spremenit eventsCtrl, da bo mel scope userMeetings al neki in ne user, in potem ustrezno pohendlat po viewjih!
 
-  STARI KONTROLERJI
-  .controller('groupDetailCtrl', ['$scope', '$routeParams', '$http',
-  function ($scope, $routeParams, $http) {
-    $http.get('data/groupDetail/'+ $routeParams.GID +'.json').success(function(data) {
-      $scope.group = data;
-    });
-  }])
-
-  .controller('groupsCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/usergroup.json').success(function(data) {
-      $scope.groups = data;
-    });
-  }])
-
-  .controller('eventsCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/usermeetings.json').success(function(data) {
-      $scope.user = data;
-    });
-  }])
-
-  .controller('profileCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/user.json').success(function(data) {
-      $scope.user = data;
-    });
-  }])
-**/
+ **/
